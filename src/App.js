d88eb528512aa67ffdee18d4ec47421dfe37a122
Priyank Bisht <priyankbish@gmail.com>
2025-05-24 10:24:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Loginsignup from './components/Loginsignup/Loginsignup';
 import Landingpage from './components/Landingpage/Landingpage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App">
       <Routes>
         <Route path='/' element = {<Loginsignup/>}></Route>
         <Route path='/land' element = {<Landingpage/>} />
       </Routes>
     </div>
-    </BrowserRouter>
+    </Router>
     
   );
 }
